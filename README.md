50246c95c1785349b76fac1d8c1628ebc42affff
Giuseppe Burtini <joe@truephp.com>
2013-01-31 14:41:00 -0800

    This is the first commit, but the code actually works.

---
+++ README.md
@@ -0,0 +1,49 @@
+# jQuery Mouseover Move
+
+jQuery Mouseover Move is a jQuery plugin which allows you to have elements which move out of the way upon mouseover. This is useful for displaying "heads up" type information without having the display element blocking or interfering with interaction with the rest of the website. It is similar to how Google's Chrome shows the URL upon mouseover a link.
+
+## Instructions
+
+1. Call jQuery("#element").mouseoverMove(options).
+2. You're done. All options are optional.
+3. At any point in time, call jQuery("#element").mouseoverMove("freeze") or jQuery("#element").mouseoverMove("unfreeze") to stop/start the transition.
+4. At any point in time, call jQuery("#element").mouseoverMove("move") to force transition.
+
+## Options
+
+```javascript
+{
+	side: '50px', // determines how far from the left/right the element sits.
+	bottom: '50px',
+	start: 'right', // or left, nothing else, determines which side of the screen it starts on
+	speed: 100 // determines how fast the animation runs
+}
+```
+
+## Screenshots
+
+Coming soon.
+
+## Known Issues
+
+* General lack of flexibility, it always locks to the bottom left/right at this point. It would be nice to be able to specify positioning more flexibly.
+* It would be nice to have other "run away" options.
+* Delayed mouse move. 
+
+## License 
+
+Please retain this notice in ALL redistributions as well as a link back to the original repository.
+
+Copyright 2013 Giuseppe Burtini      https://github.com/gburtini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this library except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ jquery-mouseover-move.js
@@ -0,0 +1,105 @@
+/*
+License - you must retain this notice in ALL redistributions
+
+Copyright 2013 Giuseppe Burtini      https://github.com/gburtini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this library except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+   
+*/
+
+(function($) {
+	var transition = function(_to) {
+		var $floaty = jQuery(this);
+
+		if(typeof _to == "undefined")
+			_to = $floaty.hasClass("right") ? "left" : "right";
+
+		var settings = $floaty.data("floaty");
+		if(settings.frozen)
+			return false;
+
+		var offsets = $floaty.offset();
+		switch(_to) {
+			case "left":
+				$floaty.css({
+					right: 'auto',
+					left: offsets.left
+				}).animate({
+					left: settings.side
+				}, settings.speed).removeClass("right").addClass("left");
+			break;
+	
+			case "right":
+				var rt = ($(window).width() - (offsets.left + $floaty.outerWidth()));
+				$floaty.css({
+					left: 'auto', 
+					right: rt
+				}).animate({
+					right: settings.side
+				}, settings.speed).removeClass("left").addClass("right");
+			break;
+		}
+	};
+
+	var methods = {
+		create: function(options) {
+			var defaults = {
+				side: '50px',
+				bottom: '50px',
+				start: 'right',         // or left, nothing else.
+				speed: 100
+			}, settings = $.extend(defaults, options);
+
+			var $floaty = jQuery(this);
+			$floaty.data("floaty", {
+				side: settings.side,
+				bottom: settings.bottom,
+				speed: settings.speed,
+				frozen: false
+			});
+		
+			var cssSettings = {
+				position: 'absolute',
+				bottom: settings.bottom
+			};
+			cssSettings[settings.start] = settings.side;
+
+			$floaty.css(cssSettings).addClass(settings.start);
+			$floaty.mouseover(function() { transition.apply($floaty) });
+		},
+
+		move: transition,
+		freeze: function() {
+			var $floaty = jQuery(this);
+			var data = $floaty.data("floaty");
+			data.frozen = true;
+			$floaty.data("floaty", data);
+		},
+		unfreeze: function() {
+			var $floaty = jQuery(this);
+			var data = $floaty.data("floaty");
+			data.frozen = false;
+			$floaty.data("floaty", data);
+		}
+	};
+
+	$.fn.mouseoverMove = function(method) {
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.create.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.mouseoverMove );
+		}   
+	};
+})(jQuery);
